8ab85ba38c915854700b2257059f97d40de4d24f
kenichi <slow_life_1407@yahoo.co.jp>
2018-11-13 12:49:53 +0000

    delete migratefile-record

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
   
   validates :content, presence: true, length: { maximum: 255 }
